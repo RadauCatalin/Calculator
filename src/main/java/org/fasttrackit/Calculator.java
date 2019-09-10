7995af4c2aadc8b9506bc5a0631291b58581cd21
Catalin <catalin.radau@yahoo.com>
2019-09-11 00:52:28 +0300

    Create calculator

---
+++ src/main/java/org/fasttrackit/Calculator.java
@@ -0,0 +1,18 @@
+package org.fasttrackit;
+
+public class Calculator {
+    String unserName;
+    int num1;
+    int num2;
+
+    public int addition(int num1, int num2) {
+        int addition = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + addition);
+        int decreases = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + decreases);
+        int multiplication = num1 * num2;
+        System.out.println(num1 + "*" + num2 + "=" + multiplication);
+
+        return addition;
+    }
+}
