c4eded93d83e9194167b31f2d69225d9ae993875
Catalin <catalin.radau@yahoo.com>
2019-09-13 12:48:39 +0300

    Re-create calculator

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,17 +4,10 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
         Calculator calculator = new Calculator();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your name");
-        calculator.unserName = scanner.nextLine();
-        System.out.println("Hello " + calculator.unserName + ",please insert first number:");
-        calculator.num1 = scanner.nextInt();
-        System.out.println("Please insert the second number");
-        calculator.num2 = scanner.nextInt();
-        System.out.println("These are your results:");
-        calculator.addition(calculator.num1, calculator.num2);
-        System.out.println("Goodbye " + calculator.unserName);
+        calculator.addition(1,2);
+        calculator.decreases(8,5);
+        calculator.multiplication(76,84);
+
     }
 }
--- src/main/java/org/fasttrackit/Calculator.java
+++ src/main/java/org/fasttrackit/Calculator.java
@@ -2,16 +2,23 @@ package org.fasttrackit;
 
 public class Calculator {
     String unserName;
-    int num1;
-    int num2;
 
     public int addition(int num1, int num2) {
         int addition = num1 + num2;
         System.out.println(num1 + "+" + num2 + "=" + addition);
-        int decreases = num1 - num2;
-        System.out.println(num1 + "-" + num2 + "=" + decreases);
-        int multiplication = num1 * num2;
-        System.out.println(num1 + "*" + num2 + "=" + multiplication);
         return addition;
     }
+
+    public int decreases(int num1, int num2) {
+        int decreases = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + decreases);
+        return decreases;
+    }
+
+    public int multiplication(int num1, int num2) {
+        int multiplication = num1 * num2;
+        System.out.println(num1 + "*" + num2 + "=" + multiplication);
+        return multiplication;
+    }
 }
+
